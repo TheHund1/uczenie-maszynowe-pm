3e3cd34b8048c5e24e854b3640ab2452f9055a15
Piotr Matuszak <matuszak.piotr@outlook.com>
2022-10-17 13:33:30 +0000

    black formatting

--- zad_1.py
+++ zad_1.py
@@ -1,11 +1,16 @@
 from statistics import mean
+
+
 class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
+
     def is_passed(self):
-        return True if(mean(self.marks) > 50) else False
-a = Student("Piotr",[1,2,3])
-b = Student("Rafał",[10000000000,2,3])
+        return True if (mean(self.marks) > 50) else False
+
+
+a = Student("Piotr", [1, 2, 3])
+b = Student("Rafał", [10000000000, 2, 3])
 print(a.is_passed())
 print(b.is_passed())
--- zad_3.py
+++ zad_3.py
@@ -4,27 +4,30 @@ class Property:
         self.rooms = rooms
         self.price = price
         self.address = address
-    
+
     def __str__(self):
-        return f'{self.area}, {self.rooms}, {self.price}, {self.address}'
-    
+        return f"{self.area}, {self.rooms}, {self.price}, {self.address}"
+
 
 class House(Property):
     def __init__(self, area, rooms: int, price, address, plot: int):
         super().__init__(area, rooms, price, address)
         self.plot = plot
-        
+
     def __str__(self):
-        return super().__str__() + ', ' + f'{self.plot}'
+        return super().__str__() + ", " + f"{self.plot}"
+
 
 class Flat(Property):
     def __init__(self, area, rooms: int, price, address, floor):
         super().__init__(area, rooms, price, address)
         self.floor = floor
-        
+
     def __str__(self):
-        return super().__str__() + ', ' + f'{self.floor}'
-House1 = House("Old Hampton",8,"56969","Bogucicka 9",5)
-Flat1 = Flat("Shire",7,"56969","Grabskiego 5b",6)
+        return super().__str__() + ", " + f"{self.floor}"
+
+
+House1 = House("Old Hampton", 8, "56969", "Bogucicka 9", 5)
+Flat1 = Flat("Shire", 7, "56969", "Grabskiego 5b", 6)
 print(House1)
 print(Flat1)
